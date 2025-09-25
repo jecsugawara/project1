d31b28c15041928655791447781015b6281413ae
sugawara <sugawara@PC43.jynet2.jec.ac.jp>
2025-09-25 16:10:25 +0900

    HelloWorldをfor文で5回表示する

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -3,8 +3,9 @@ package project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
+		for(int i=0; i<5; i++) {
+			System.out.println("HelloWorld");
+		}
 	}
 
 }
