e479725b66c5a9eb17e52f37bdeadf832bb68fdc
24jy0000 Denshi Taro <24jy0000@jec.ac.jp>
2025-10-09 13:26:12 +0900

    22

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -5,7 +5,5 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
 	}
-
 }
